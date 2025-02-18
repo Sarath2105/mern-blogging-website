0dff62a08eb229be14ee45249c2016a3586e3a1a
Sarath Mukundhan <your-email@example.com>
2025-02-18 10:31:03 +0530

    Update .gitignore, re-add .png files, and commit modified components

---
+++ blogging website - frontend/src/components/navbar.component.jsx
@@ -0,0 +1,13 @@
+import logo from "../imgs/logo.png"
+
+const navbar = () => {
+    return(
+        <nav class="navbar">
+            <img src="logo"/>
+
+        </nav>
+    )
+
+}
+
+export default navbar;
